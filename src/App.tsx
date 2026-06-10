87283156e4bab5dd752cf52cb99036dec4e13d8a
Vanesa Robledo <v.robledo@vehikl.com>
2026-06-10 09:43:22 -0400

    update refs to useState for all input fields, create generic updateField function that uses onChange event

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,35 @@
-import {useState, createRef} from 'react'
+import {useState} from 'react'
 import {generateList} from './utils/rng.ts'
 import InputNumber from "./components/InputNumber.tsx"
 
 function App() {
     const [errors, setFormErrors] = useState('')
 
-    const minimumNumberField = createRef<HTMLInputElement>()
-    const maximumNumberField = createRef<HTMLInputElement>()
-    const numTimesField = createRef<HTMLInputElement>()
-    const delaySecondsField = createRef<HTMLInputElement>()
-    const countdownSecondsField = createRef<HTMLInputElement>()
+    type FormState = {
+        minimumNumber: number;
+        maximumNumber: number;
+        numTimes: number;
+        delaySeconds: number;
+        countdownSeconds: number;
+    };
+
+    const [form, setForm] = useState<FormState>({
+        minimumNumber: 1,
+        maximumNumber: 5,
+        numTimes: 5,
+        delaySeconds: 1,
+        countdownSeconds: 1,
+    });
+
+    function updateField<K extends keyof FormState>(
+        field: K,
+        value: FormState[K]
+    ) {
+        setForm(prev => ({
+            ...prev,
+            [field]: value,
+        }))
+    }
 
     const [numbers, setNumbers] = useState('');
 
@@ -28,9 +48,9 @@ function App() {
     }
 
     const start = () => {
-        const minimumNumber = Number(minimumNumberField.current?.value)
-        const maximumNumber = Number(maximumNumberField.current?.value);
-        const numTimes = Number(numTimesField.current?.value);
+        const minimumNumber = Number(form.minimumNumber)
+        const maximumNumber = Number(form.maximumNumber);
+        const numTimes = Number(form.numTimes);
 
         if (!validateNums(minimumNumber, maximumNumber)) {
             console.log("Not successful");
@@ -39,37 +59,59 @@ function App() {
 
         const randomNumbers = generateList(numTimes, minimumNumber, maximumNumber, true)
         setNumbers(randomNumbers.toString());
-    }
+    };
 
     return (
-        <form action={start}>
-            <header><h1>Text-to-Speech Random Number Generator</h1></header>
-            <main>
-                <section id="range">
-                    Enter a range:
-                    <InputNumber name="minimumNumber" label="Minimum:" defaultValue={1} min={1} max={99} step={1}
-                                          unit="" ref={minimumNumberField}/>
-                    <InputNumber name="maximumNumber" label="Maximum:" defaultValue={5} min={2} max={99} step={1}
-                                          unit="" ref={maximumNumberField}/>
+        <form
+            onSubmit={(e) => {
+                e.preventDefault();
+                start();
+            }
+            }>
+            <header><h1 className="text-5xl mb-6">Text-to-Speech Random Number Generator</h1></header>
+            <main className="grid grid-cols-2 text-left">
+                <section>
+                    <section id="range">
+                        Enter a range:
+                        <div className="flex">
+                            <InputNumber name="minimumNumber" label="Minimum:" defaultValue={1} min={1} max={99}
+                                         step={1} unit=""
+                                         onChange={(e) =>
+                                             updateField('minimumNumber', Number(e.target.value))}/>
+                            <InputNumber name="maximumNumber" label="Maximum:" defaultValue={5} min={2} max={99}
+                                         step={1} unit=""
+                                         onChange={(e) =>
+                                             updateField('maximumNumber', Number(e.target.value))}/>
+
+                        </div>
+                    </section>
+                    <section id="generate">
+                        <InputNumber name="numTimes" label="Generate" defaultValue={5} min={1} max={99} step={1}
+                                     unit="times"
+                                     onChange={(e) =>
+                                         updateField('numTimes', Number(e.target.value))}/>
+                    </section>
+                    <section id="delay">
+                        <InputNumber name="delaySeconds" label="Delay" defaultValue={1.00} min={0.25} max={5.0}
+                                     step={0.25} unit="seconds"
+                                     onChange={(e) =>
+                                         updateField('delaySeconds', Number(e.target.value))}/>
+                    </section>
+                    <section id="countdown">
+                        <InputNumber name="countdownSeconds" label="Countdown" defaultValue={1.00} min={0.0} max={5.0}
+                                     step={0.25} unit="seconds"
+                                     onChange={(e) =>
+                                         updateField('countdownSeconds', Number(e.target.value))}/>
+                    </section>
+                    <section id="startButton">
+                        <button
+                            className="p-2 m-2 border border-gray-200 bg-neutral-50 rounded text-2xl hover:bg-neutral-500 hover:text-white">Start
+                        </button>
+                        <p><span className="text-red-400">{errors}</span></p>
+                    </section>
                 </section>
-                <section id="generate">
-                    <InputNumber name="numTimes" label="Generate" defaultValue={5} min={1} max={99} step={1}
-                                 unit="times" ref={numTimesField}/>
-                </section>
-                <section id="delay">
-                    <InputNumber name="delaySeconds" label="Delay" defaultValue={1.00} min={0.25} max={5.0} step={0.25}
-                                 unit="seconds" ref={delaySecondsField}/>
-                </section>
-                <section id="countdown">
-                    <InputNumber name="countdownSeconds" label="Countdown" defaultValue={1.00} min={0.0} max={5.0} step={0.25}
-                                 unit="seconds" ref={countdownSecondsField}/>
-                </section>
-                <section id="startButton">
-                    <button className="p-2 m-2 border border-gray-200 bg-neutral-50 rounded text-2xl hover:bg-neutral-500 hover:text-white">Start</button>
-                    <p><span className="error">{errors}</span></p>
-                </section>
-                <section id="numDisplay">
-                    {numbers}
+                <section id="numDisplay" className="border border-neutral-100 flex content-center">
+                    <div className="m-auto text-6xl">{numbers}</div>
                 </section>
             </main>
         </form>
--- src/components/InputNumber.tsx
+++ src/components/InputNumber.tsx
@@ -1,6 +1,6 @@
-import type {RefObject} from "react";
+import type {ChangeEventHandler} from "react";
 
-export default function InputNumber({name, label, unit, defaultValue, min, max, step, ref}: {
+export default function InputNumber({name, label, unit, defaultValue, min, max, step, onChange}: {
     name: string,
     label: string,
     unit: string,
@@ -8,14 +8,15 @@ export default function InputNumber({name, label, unit, defaultValue, min, max, 
     min: number,
     max: number,
     step: number,
-    ref: RefObject<HTMLInputElement | null>,
+    onChange: ChangeEventHandler<HTMLInputElement>
 }) {
     return (
-        <div className="max-w-xs mx-auto">
-            <label htmlFor={name} className="text-sm text-black">{label}</label>
-            <input type="number" name={name} defaultValue={defaultValue} min={min} max={max} step={step} ref={ref}
+        <div className="">
+            <label htmlFor={name} className="text-sm text-neutral-800">{label}</label>
+            <input type="number" name={name} defaultValue={defaultValue} min={min} max={max} step={step}
+                   onChange={ onChange }
                 className="p-2 m-2 border border-gray-200 rounded text-lg" />
-            { unit }
+            <span className="text-sm text-neutral-600">{ unit }</span>
         </div>
     )
 }
